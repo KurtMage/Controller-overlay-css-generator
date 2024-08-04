1e495b82de5c5247811db6dbbbb261e1896cf41b
KurtMage <kmaegerle@gmail.com>
2024-08-03 20:33:29 -0700

    Working for all buttons, just need to add start/back

--- drag.js
+++ drag.js
@@ -37,57 +37,34 @@ function dragDiv(e) {
 	// move div element
 	targ.style.left=coordX+e.clientX-offsetX+'px';
 	targ.style.top=coordY+e.clientY-offsetY+'px';
-	// console.log("kurttm debug top: " + targ.style.top);
 	return false;
 }
 function stopDrag() {
 	drag=false;
 
-	var changedVariables = "";
+	var changedVariables = "body { background-color: rgba(0, 0, 0, 0); margin: 0px auto; overflow: hidden; }<br>";
 	for (const img of document.getElementsByTagName('span')) {
-		console.log("kurttm debug top: " + img.style.top);
-		console.log("kurttm debug left: " + img.style.left);
-		// TODO (and not zero? or probably not unchanged);
-		// if (img.style.top) {
-		// 	changedVariables += img.id + "    top: " + img.style.top + ";<br>";
-		// }
-		// if (img.style.left) {
-		// 	changedVariables += img.id + "    left: " + img.style.left + ";<br>";
-		// }
 		if (img.style.top || img.style.left) {
 			changedVariables +=
 			`
 			<br>${img.id} {<br>
-				${img.style.top ? `top: ${img.style.top}<br>` : ''}
-				${img.style.left ? `left: ${img.style.left}<br>` : ''}
+				${img.style.top ? `top: ${img.style.top};<br>` : ''}
+				${img.style.left ? `left: ${img.style.left};<br>` : ''}
 			}<br>
 			`
 		}
 	}
 	
 	document.getElementById("css-text1").innerHTML =changedVariables;
-		// `:root {<br>
-		// 	${changedVariables}
-		// }`;
 
 	document.getElementById("css-text").value =changedVariables;
-		// `:root {<br>
-		// 	${changedVariables}
-		// }`;
-	console.log("kurttm debug top: " + targ.style.top);
 }
 
 
 function copyText() {
   // Get the text field
-  console.log("kurttm debug copyText start");
   var copyText = document.getElementById("css-text1").innerText;
 
-  // Select the text field
-//   copyText.select();
-//   copyText.setSelectionRange(0, 99999); // For mobile devices
-
-  console.log("kurttm debug copyText: " + copyText);
   // Copy the text inside the text field
   navigator.clipboard.writeText(copyText);
 }
